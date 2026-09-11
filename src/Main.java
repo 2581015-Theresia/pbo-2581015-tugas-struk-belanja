59ff9b5c457c514016246943e738820982e51b62
Theresia Angel Simbolon <2581015@unai.edu>
2026-09-11 09:49:21 +0700

    Menambahkan barang 1

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        // Barang 1
+        System.out.print("Nama barang 1   : ");
+        String namaBarang1 = scanner.nextLine();
+
+        System.out.print("Harga satuan    : ");
+        int harga1 = scanner.nextInt();
+
+        System.out.print("Jumlah          : ");
+        int jumlah1 = scanner.nextInt();
+
+        scanner.nextLine();
